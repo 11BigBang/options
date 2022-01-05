a251c7210e3752f3fff6cf85222e5d3b85f82d4e
11BigBang <wbmartin172@gmail.com>
2022-01-05 07:04:07 -0700

    Modified calendar functions

--- utils.py
+++ utils.py
@@ -25,7 +25,23 @@ def get_fridays(start=date(date.today().year, 1, 1), end=date.today()):
     return fri_list
 
 def get_weekdays(start=date(date.today().year, 1, 1), end=date.today()):
-    pass
+    if not isinstance(start, date):
+        start = date.fromisoformat(start)
+    if not isinstance(end, date):
+        end = date.fromisoformat(end)
+
+    weekday_list = []
+    temp = start
+    while temp <= end:
+        if temp.weekday() < 5:
+            weekday_list.append(temp)
+        temp += timedelta(days=1)
+
+    return weekday_list
+
+# w = get_weekdays(start='2021-08-01', end='2021-08-12')
+# for i in w:
+#     print(i)
 
 # get_fridays(start='2021-02-13', end='2021-08-12')
 # get_fridays()

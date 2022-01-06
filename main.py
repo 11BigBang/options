3f34ddd3bfdea4825d2f379ce41ff57c3ff6e9ff
11BigBang <wbmartin172@gmail.com>
2022-01-05 21:16:48 -0700

    Added more dates as arguments

--- main.py
+++ main.py
@@ -1,3 +1,8 @@
 from scrape import ScrapeChain
 
-scrape = ScrapeChain(start='2021-02-13', end='2021-08-12')
+scrape = ScrapeChain(
+        ex_start='2021-02-13',
+        ex_end='2021-08-12',
+        day_start='2021-02-13',
+        day_end='2021-08-12'
+        )

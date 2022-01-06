1630aa8712c61389a5cbd42b775ca1d28a91bb6d
11BigBang <wbmartin172@gmail.com>
2022-01-05 21:17:53 -0700

    Added daily part of URL and prepared for db insertion

--- scrape.py
+++ scrape.py
@@ -8,10 +8,16 @@ from datetime import timedelta
 from utils import get_fridays, get_weekdays
 
 class ScrapeChain:
-    def __init__(self, start, end):
-        # conn = sqlite3.connect('options.db')
-        #
-        # c = conn.cursor()
+    """Scrapes options chains.
+
+    Arguments
+    - ex_start: The start date for the expiry dates to scrape.
+    - ex_end: The end date for the expiry dates to scrape.
+    """
+    def __init__(self, ex_start, ex_end, day_start, day_end):
+        conn = sqlite3.connect('options.db')
+
+        c = conn.cursor()
 
         options = Options()
         options.add_argument('--headless')
@@ -20,24 +26,27 @@ class ScrapeChain:
         driver = webdriver.Chrome(service=service, options=options)
         time.sleep(5)
 
-        self.start = start
-        self.end = end
+        self.ex_start = ex_start
+        self.ex_end = ex_end
+        self.day_start = day_start
+        self.day_end = day_end
 
         # example URL 'https://omnieq.com/underlyings/NYSE/GME/chain/2019/03/22/historical/2019/03/22'
         URL_1 = 'https://omnieq.com/underlyings/NYSE/GME/chain/'
         URL_3 = '/historical/'
+        row = []
 
-        fri_list = get_fridays(start=self.start, end=self.end)
-        weekday_list = get_weekdays(start=self.start, end=self.end)
+        fri_list = get_fridays(start=self.ex_start, end=self.ex_end)
+        weekday_list = get_weekdays(start=self.day_start, end=self.day_end)
 
         for expiry in fri_list:
-            driver.get(f"{URL_1}+{expiry.strftime('%Y/%m/%d')}")
+            driver.get(f"{URL_1}{expiry.strftime('%Y/%m/%d')}")
             # td_list allows for timedelta values that correspond to Thursday, Wednesday,
             # Monday, and Tuesday surrounding the typical Friday expiry
             search_ct, td_list = 0, [-1, -1, 5, 1]
             while driver.title == 'Page Not Found' and search_ct < 5:
                 expiry += timedelta(days=td_list[search_ct])
-                driver.get(f"{URL_1}+{expiry.strftime('%Y/%m/%d')}")
+                driver.get(f"{URL_1}{expiry.strftime('%Y/%m/%d')}")
                 search_ct += 1
 
             if driver.title == 'Page Not Found':
@@ -46,11 +55,23 @@ class ScrapeChain:
             if expiry.weekday() != 4:
                 print(expiry)
 
-            # empty_ct = 0
-            # # driver.get(URL_1+)
-            # elem = driver.find_elements(By.TAG_NAME, 'td')
-            # for i in elem:
-            #     print(i.text)
+            for weekday in weekday_list:
+                driver.get(f"{URL_1}{expiry.strftime('%Y/%m/%d')}{URL_3}{weekday.strftime('%Y/%m/%d')}")
+                if driver.title != 'Page Not Found':
+                    data = driver.find_elements(By.TAG_NAME, 'td')
+                    ct = 0
+                    for datum in data:
+                        row.append(datum.text)
+                        if ct % 14 == 0:
+                            c.execute(
+                                #TODO: change below to insert into table and modify creation.py to reflect
+                                "INSERT INTO NYSE_Threshold_Securities "
+                                "(Symbol, Security_Name, Trade_Date, Market)"
+                                " VALUES(%s,%s,%s,%s)",
+                                row)
+                            row = []
+
+                        ct += 1
 
         driver.quit()
 

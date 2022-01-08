6333ee2fb6f143d411674ef4d3c767d09970699f
11BigBang <wbmartin172@gmail.com>
2022-01-08 09:15:04 -0700

    Changed parameters

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 from scrape import ScrapeChain
 
 scrape = ScrapeChain(
-                    ex_start='2021-02-13',
+                    ex_start='2021-07-27',
                     ex_end='2021-08-12',
-                    day_start='2021-02-13',
+                    day_start='2021-07-27',
                     day_end='2021-08-12'
                     )

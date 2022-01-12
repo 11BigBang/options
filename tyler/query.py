786d3bc65565603bbc97e2fb8380ed7c5528da5e
11BigBang <wbmartin172@gmail.com>
2022-01-12 08:33:07 -0700

    Finished get_data function

--- tyler/query.py
+++ tyler/query.py
@@ -1,12 +1,22 @@
-import sqlite3
+import sqlite3, time
+from datetime import datetime as dt
 
-conn = sqlite3.connect('../options.db')
 
-c = conn.cursor()
+def get_data(metric, start, end):
+    start_unix = time.mktime(dt.strptime(start, "%Y-%m-%d").timetuple())
+    end_unix = time.mktime(dt.strptime(end, "%Y-%m-%d").timetuple())
 
-statement = ''
-c.execute(statement)
+    conn = sqlite3.connect('../options.db')
+    c = conn.cursor()
 
-conn.commit()
+    c.execute("SELECT date, ? FROM gme WHERE date BETWEEN ? AND ?", (metric, start_unix, end_unix))
+    data = c.fetchall()
+    conn.close()
 
-conn.close()
+    unix_list = [item[0] for item in data]
+    date_list = [dt.fromtimestamp(int(ts)) for ts in unix_list] # Converts Unix timestamp to local datetime
+    last_list = [item[1] for item in data]
+
+    return(date_list, last_list)
+
+

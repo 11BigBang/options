c2a9c0ab7f0fb758b76549cc80797c8ff8f5ee38
11BigBang <wbmartin172@gmail.com>
2022-01-13 20:53:36 -0700

    Temporarily added run time

--- bryant/main.py
+++ bryant/main.py
@@ -1,5 +1,8 @@
 from scrape import ScrapeChain
+from datetime import datetime as dt
 
+begin = dt.now()
 scrape = ScrapeChain(start='2021-07-27', end='2021-07-27')
 scrape.driver.quit()
 scrape.conn.close()
+print(f'Run time was {dt.now() - begin}')

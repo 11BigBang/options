7aea3b9331daa7b5501dffb3f71fbb86f03c2bcd
11BigBang <wbmartin172@gmail.com>
2022-01-14 07:07:32 -0700

    New dates for scrape

--- bryant/main.py
+++ bryant/main.py
@@ -2,7 +2,7 @@ from scrape import ScrapeChain
 from datetime import datetime as dt
 
 begin = dt.now()
-scrape = ScrapeChain(start='2021-07-27', end='2021-07-27')
+scrape = ScrapeChain(start='2020-12-01', end='2020-12-14')
 scrape.driver.quit()
 scrape.conn.close()
 print(f'Run time was {dt.now() - begin}')

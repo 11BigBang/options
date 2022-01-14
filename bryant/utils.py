00b5b6ae021db3b19999cc129434d441c9e781e8
11BigBang <wbmartin172@gmail.com>
2022-01-13 20:54:46 -0700

    Deleted get_fridays function

--- bryant/utils.py
+++ bryant/utils.py
@@ -1,28 +1,6 @@
 from datetime import date, timedelta
 from datetime import datetime as dt
 
-def get_fridays(start=date(date.today().year, 1, 1), end=date.today()):
-    """Creates a list of dates for every Friday within timeframe.
-
-    Arguments:
-        start - format: '%Y/%m/%d'
-        end
-            format:  '%Y/%m/%d'
-            default: current date
-    """
-    if not isinstance(start, date):
-        start = date.fromisoformat(start)
-    if not isinstance(end, date):
-        end = date.fromisoformat(end)
-
-    fri_list = []
-    temp = start + timedelta(days = (4 - start.weekday() + 7) % 7)
-
-    while temp <= end:
-        fri_list.append(temp)
-        temp += timedelta(days=7)
-
-    return fri_list
 
 def get_weekdays(start=date(date.today().year, 1, 1), end=date.today()):
     if not isinstance(start, date):

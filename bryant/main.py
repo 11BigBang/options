112b176d07dcc1de3d4a1f914dd79c0ac7d411aa
11BigBang <wbmartin172@gmail.com>
2022-01-16 14:11:45 -0700

    Scrape start='2021-12-20', end='2021-12-24'

--- bryant/main.py
+++ bryant/main.py
@@ -3,7 +3,7 @@ from datetime import datetime as dt
 
 begin = dt.now()
 print(begin)
-scrape = ScrapeChain(start='2021-12-13', end='2021-12-17')
+scrape = ScrapeChain(start='2021-12-20', end='2021-12-24')
 scrape.driver.quit()
 scrape.conn.close()
 print(f'Finished {dt.now()}, Run time was {dt.now() - begin}')
